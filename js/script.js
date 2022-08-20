db0c2e0530ce4198d4100b2a87b848a7cf80f60f
AlexanderIvanov <alexivanov2890@mail.ru>
2022-08-21 01:02:11 +0300

    finished media header and added menu-mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const tabsBtn = document.querySelectorAll(".testimonials__tab");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu-mobile");
+const closeElem = document.querySelector(".menu-mobile__close");
 
 tabsBtn.forEach(function(item) {
     item.addEventListener("click", function() {
@@ -10,4 +13,12 @@ tabsBtn.forEach(function(item) {
 
         cuurentBtn.classList.add("testimonials__tab_active");
     });
+});
+
+hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
+});
+
+closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
 });
